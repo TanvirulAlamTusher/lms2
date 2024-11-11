33dbacdcfa9d2ae2a5ccfce44c933aed15b1bf64
TanvirulAlamTusher <tanvirulalam15@gmail.com>
2024-11-11 23:16:06 +0600

    set smtp data in app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+// use Config;
+use App\Models\SmtpSetting;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+       if(Schema::hasTable('smtp_settings')){
+        $smtpsetting = SmtpSetting::first();
+
+        if($smtpsetting){
+          $data = [
+            'driver' => $smtpsetting->mailer,
+            'host' => $smtpsetting->host,
+            'port' => $smtpsetting->port,
+            'username' => $smtpsetting->username,
+            'password' => $smtpsetting->password,
+            'encryption' => $smtpsetting->encryption,
+            'from' =>[
+                'address' => $smtpsetting->from_address,
+                'name' => 'Easy Learning',
+            ]
+            ];
+            Config::set('mail', $data);
+        }
+
+       }// end if
+
+
+
     }
 }
