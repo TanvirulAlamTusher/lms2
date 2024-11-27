36a6b3701fdfbd3fd073bb4f8e2720212154c81c
TanvirulAlamTusher <tanvirulalam15@gmail.com>
2024-11-27 11:27:51 +0600

    add header and footer in course view balde

--- resources/views/frontend/mycourse/course_view.blade.php
+++ resources/views/frontend/mycourse/course_view.blade.php
@@ -1,33 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8">
-    <meta name="author" content="TechyDevs">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>Course Details</title>
+@include('frontend.mycourse.body.header')
 
-    <!-- Google fonts -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet">
 
-    <!-- Favicon -->
-    <link rel="icon" sizes="16x16" href="{{ asset('frontend/images/favicon.png') }}">
-
-    <!-- inject:css -->
-    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/css/line-awesome.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/css/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/css/owl.theme.default.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap-select.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/css/fancybox.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/css/animated-headline.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/css/plyr.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/css/jquery-te-1.4.0.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
-    <!-- end inject -->
-</head>
 <body>
 
 <!-- start cssload-loader -->
@@ -1732,24 +1708,8 @@
     </div><!-- end modal-dialog -->
 </div><!-- end modal -->
 
-<!-- template js files -->
 
-<script src="{{ asset('frontend/js/jquery-3.4.1.min.js') }}"></script>
-<script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('frontend/js/bootstrap-select.min.js') }}"></script>
-<script src="{{ asset('frontend/js/owl.carousel.min.js') }}"></script>
-<script src="{{ asset('frontend/js/isotope.js') }}"></script>
-<script src="{{ asset('frontend/js/waypoint.min.js') }}"></script>
-<script src="{{ asset('frontend/js/jquery.counterup.min.js') }}"></script>
-<script src="{{ asset('frontend/js/fancybox.js') }}"></script>
-<script src="{{ asset('frontend/js/plyr.js') }}"></script>
-<script src="{{ asset('frontend/js/datedropper.min.js') }}"></script>
-<script src="{{ asset('frontend/js/emojionearea.min.js') }}"></script>
-<script src="{{ asset('frontend/js/jquery-te-1.4.0.min.js') }}"></script>
-<script src="{{ asset('frontend/js/jquery.MultiFile.min.js') }}"></script>
-<script src="{{ asset('frontend/js/main.js') }}"></script>
-<script>
-    var player = new Plyr('#player');
-</script>
+@include('frontend.mycourse.body.footer')
+
 </body>
 </html>
