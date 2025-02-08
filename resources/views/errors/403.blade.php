b6a6dc7dfd8b133f25293a2b7e55856aae741f77
TanvirulAlamTusher <tanvirulalam15@gmail.com>
2025-02-08 21:18:54 +0600

    add 404 and  403 page design

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--favicon-->
+	<link rel="icon" href="assets/images/favicon-32x32.png" type="image/png" />
+	<!-- loader-->
+	<link href="{{ asset('backend/assets/css/pace.min.css') }}" rel="stylesheet" />
+	<script src="{{ asset('backend/assets/js/pace.min.js') }}"></script>
+	<!-- Bootstrap CSS -->
+	<link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" rel="stylesheet">
+	<link href="{{ asset('backend/assets/css/bootstrap-extended.css') }}" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+	<link href="{{ asset('backend/assets/css/app.css') }}" rel="stylesheet">
+	<link href="{{ asset('backend/assets/css/icons.css') }}" rel="stylesheet">
+	<title>403 No Permission</title>
+</head>
+
+<body>
+	<!-- wrapper -->
+	<div class="wrapper">
+		<nav class="navbar navbar-expand-lg navbar-light bg-white rounded fixed-top rounded-0 shadow-sm">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">
+					<img src="assets/images/logo-img.png" width="140" alt="" />
+				</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span>
+				</button>
+
+			</div>
+		</nav>
+		<div class="error-404 d-flex align-items-center justify-content-center">
+			<div class="container">
+				<div class="card py-5">
+					<div class="row g-0">
+						<div class="col col-xl-5">
+							<div class="card-body p-4">
+								<h1 class="display-1"><span class="text-primary">4</span><span class="text-danger">0</span><span class="text-success">3</span></h1>
+								<h2 class="font-weight-bold display-4">USER DOESNOT HAVE THE RIGHT PERMISSIONS</h2>
+								<p>You have reached the edge of the universe.
+									<br>The page you requested could not be found.
+									<br>Dont'worry and return to the previous page.</p>
+								<div class="mt-5"> <a href="javascript:;" class="btn btn-primary btn-lg px-md-5 radius-30">Go Home</a>
+									<a href="javascript:;" class="btn btn-outline-dark btn-lg ms-3 px-md-5 radius-30">Back</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-xl-7">
+							<img src="https://cdn.searchenginejournal.com/wp-content/uploads/2019/03/shutterstock_1338315902.png" class="img-fluid" alt="">
+						</div>
+					</div>
+					<!--end row-->
+				</div>
+			</div>
+		</div>
+		<div class="bg-white p-3 fixed-bottom border-top shadow">
+			<div class="d-flex align-items-center justify-content-between flex-wrap">
+				<ul class="list-inline mb-0">
+					<li class="list-inline-item">Follow Us :</li>
+					<li class="list-inline-item"><a href="javascript:;"><i class='bx bxl-facebook me-1'></i>Facebook</a>
+					</li>
+					<li class="list-inline-item"><a href="javascript:;"><i class='bx bxl-twitter me-1'></i>Twitter</a>
+					</li>
+					<li class="list-inline-item"><a href="javascript:;"><i class='bx bxl-google me-1'></i>Google</a>
+					</li>
+				</ul>
+				<p class="mb-0">Copyright © 2021. All right reserved.</p>
+			</div>
+		</div>
+	</div>
+	<!-- end wrapper -->
+	<!-- Bootstrap JS -->
+	<script src="{{ asset('backend/assets/js/bootstrap.bundle.min.js') }}"></script>
+</body>
+
+</html>
